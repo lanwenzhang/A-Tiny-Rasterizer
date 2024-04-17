abcce8ba67baff723b9e07c96c56f74896d70afa
Lanwen Zhang <lanwenzhang52@gmail.com>
2024-04-17 21:28:36 +0200

    Update main.cpp

--- Step 1 Bresenham’s Line Algorithm/main.cpp
+++ Step 1 Bresenham’s Line Algorithm/main.cpp
@@ -1,9 +1,10 @@
+#include <cmath>
 #include "tgaimage.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red   = TGAColor(255, 0,   0,   255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
 
-void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) 
+void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
 {
 	// Determine whether y is more steep than x
 	bool steep = false;
@@ -20,11 +21,11 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
 		std::swap(x0, x1);
 		std::swap(y0, y1);
 	}
-	
+
 	// Decrease the interporlation error
 	int dx = x1 - x0;
 	int dy = y1 - y0;
-	float derror2 = std::abs(dy)*2;
+	float derror2 = std::abs(dy) * 2;
 	float error2 = 0;
 	int y = y0;
 	for (float x = x0; x <= x1; x++)
@@ -51,9 +52,7 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
 
 int main(int argc, char** argv) {
 	TGAImage image(100, 100, TGAImage::RGB);
-	line(13, 20, 80, 40, image, white);
-	line(20, 13, 40, 80, image, red);
-	line(80, 40, 13, 20, image, red);
+	line(20, 13, 40, 80, image, white);
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
 	return 0;
